34cabe52b60132867c23a5688b4026aa849af1e7
DancerVlt69 <dancervlt69@googlemail.com>
2022-12-14 02:15:01 +0100

    updated/removed: ModTabs/CreativeModeTabs from the Item Registration

--- src/main/java/net/dancervlt69/slabsnstairs/Init/Items/ModItems.java
+++ src/main/java/net/dancervlt69/slabsnstairs/Init/Items/ModItems.java
@@ -3,7 +3,6 @@ package net.dancervlt69.slabsnstairs.Init.Items;
 import net.dancervlt69.slabsnstairs.Init.Blocks.ModBlocks;
 import net.dancervlt69.slabsnstairs.Init.Items.Custom.AdvancedItem;
 import net.dancervlt69.slabsnstairs.Init.Items.Custom.Utils.ModItemTiers;
-import net.dancervlt69.slabsnstairs.Init.Tabs.ModTabs;
 import net.dancervlt69.slabsnstairs.SlabsNstairs;
 import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -23,26 +22,27 @@ public class ModItems {
         // Dye
     @Deprecated
     public static final RegistryObject<Item> LIGHT_BROWN_DYE = MOD_ITEMS.register("light_brown_dye",
-            () -> new DyeItem(DyeColor.BROWN,(new Item.Properties().tab(ModTabs.SNSMODTAB3))));
+            () -> new DyeItem(DyeColor.BROWN,(new Item.Properties())));
 
     public static final RegistryObject<Item> CITRINE = MOD_ITEMS.register("citrine",
-            () -> new Item(new Item.Properties().tab(ModTabs.SNSMODTAB3)));
+            () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<AdvancedItem> CITRINE_COIN = MOD_ITEMS.register("citrine_coin",
-            () -> new AdvancedItem(new Item.Properties().tab(ModTabs.SNSMODTAB3)));
+            () -> new AdvancedItem(new Item.Properties()));
 
     public static final RegistryObject<Item> CINNAMON_SIGN = MOD_ITEMS.register("cinnamon_sign",
-            () -> new SignItem(new Item.Properties().tab(ModTabs.SNSMODTAB1).stacksTo(16),
+            () -> new SignItem(new Item.Properties().stacksTo(16),
                     ModBlocks.CINNAMON_SIGN.get(), ModBlocks.CINNAMON_WALL_SIGN.get()));
 
     @Deprecated
     public static final RegistryObject<Item> CITRINE_SWORD = MOD_ITEMS.register("citrine_sword",
             () -> new SwordItem(ModItemTiers.CITRINE,5,1.0f,
-                    new Item.Properties().tab(ModTabs.SNSMODTAB2)));
+                    new Item.Properties()));
 
     //Register the defined Item/s
     public static void register(IEventBus eventBus){
         SlabsNstairs.LOGGER.debug("Registering Mod-Items for " + MODID);
         MOD_ITEMS.register(eventBus);
     }
+
 }
