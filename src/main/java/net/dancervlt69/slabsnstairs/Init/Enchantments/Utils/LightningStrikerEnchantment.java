a9eb8c58ca04f0edeff8a1675d06fd793e88d6d1
DancerVlt69 <dancervlt69@googlemail.com>
2022-12-14 02:04:02 +0100

    updated: the Lightning Striker Enchantment to MC 1.19.3

--- src/main/java/net/dancervlt69/slabsnstairs/Init/Enchantments/Utils/LightningStrikerEnchantment.java
+++ src/main/java/net/dancervlt69/slabsnstairs/Init/Enchantments/Utils/LightningStrikerEnchantment.java
@@ -3,6 +3,7 @@ package net.dancervlt69.slabsnstairs.Init.Enchantments.Utils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.*;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
@@ -17,21 +18,21 @@ public class LightningStrikerEnchantment extends Enchantment {
             ServerLevel world = ((ServerLevel) pAttacker.level);
             BlockPos blockPos = pTarget.blockPosition();
             if (pLevel == 1) {
-                EntityType.LIGHTNING_BOLT.spawn(world, null, null, blockPos,
+                EntityType.LIGHTNING_BOLT.spawn(world, (ItemStack) null, null, blockPos,
                         MobSpawnType.TRIGGERED, true, true);
             }
             if (pLevel == 2) {
-                EntityType.LIGHTNING_BOLT.spawn(world, null, null, blockPos,
+                EntityType.LIGHTNING_BOLT.spawn(world, (ItemStack) null, null, blockPos,
                         MobSpawnType.TRIGGERED, true, true);
-                EntityType.LIGHTNING_BOLT.spawn(world, null, null, blockPos,
+                EntityType.LIGHTNING_BOLT.spawn(world, (ItemStack) null, null, blockPos,
                         MobSpawnType.TRIGGERED, true, true);
             }
             if (pLevel == 3) {
-                EntityType.LIGHTNING_BOLT.spawn(world, null, null, blockPos,
+                EntityType.LIGHTNING_BOLT.spawn(world, (ItemStack) null, null, blockPos,
                         MobSpawnType.TRIGGERED, true, true);
-                EntityType.LIGHTNING_BOLT.spawn(world, null, null, blockPos,
+                EntityType.LIGHTNING_BOLT.spawn(world, (ItemStack) null, null, blockPos,
                         MobSpawnType.TRIGGERED, true, true);
-                EntityType.LIGHTNING_BOLT.spawn(world, null, null, blockPos,
+                EntityType.LIGHTNING_BOLT.spawn(world, (ItemStack) null, null, blockPos,
                         MobSpawnType.TRIGGERED, true, true);
             }
         }
