c57e64ad44c3a1b837c5dded5057aa14e320cb60
DancerVlt69 <dancervlt69@googlemail.com>
2022-12-14 02:32:01 +0100

    disabled: Ore-Generation (temporarily)

--- src/main/java/net/dancervlt69/slabsnstairs/Init/World/Features/ModConfiguredFeatures.java
+++ src/main/java/net/dancervlt69/slabsnstairs/Init/World/Features/ModConfiguredFeatures.java
@@ -2,13 +2,14 @@ package net.dancervlt69.slabsnstairs.Init.World.Features;
 
 import com.google.common.base.Suppliers;
 import net.dancervlt69.slabsnstairs.Init.Blocks.ModBlocks;
-import net.minecraft.core.Registry;
-import net.minecraft.data.worldgen.features.OreFeatures;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import static net.dancervlt69.slabsnstairs.SlabsNstairs.MODID;
 
@@ -19,16 +20,20 @@ import java.util.function.Supplier;
 
 public class ModConfiguredFeatures {
 
-    public static final DeferredRegister<ConfiguredFeature<?, ?>> CONFIGURED_FEATURES =
-            DeferredRegister.create(Registry.CONFIGURED_FEATURE_REGISTRY, MODID);
+//    public static final DeferredRegister<ConfiguredFeature<?, ?>> CONFIGURED_FEATURES =
+//          DeferredRegister.create(ForgeRegistries.FEATURES.getRegistryName(), MODID);
 
-    public static final Supplier<List<OreConfiguration.TargetBlockState>> OVERWORLD_CITRINE_ORES = Suppliers.memoize(()
-            -> List.of(
-            OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.CITRINE_ORE.get().defaultBlockState()),
-            OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.DEEPSLATE_CITRINE_ORE.get().defaultBlockState())));
+//    public static final Supplier<List<OreConfiguration.TargetBlockState>> OVERWORLD_CITRINE_ORES = Suppliers.memoize(()
+//            -> List.of(
+//            OreConfiguration.target(new TagMatchTest(BlockTags.BASE_STONE_OVERWORLD),
+//                    ModBlocks.CITRINE_ORE.get().defaultBlockState()),
+//            OreConfiguration.target(new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES),
+//                    ModBlocks.DEEPSLATE_CITRINE_ORE.get().defaultBlockState())));
+//
+//    public static final RegistryObject<ConfiguredFeature<?, ?>> CITRINE_ORE = CONFIGURED_FEATURES.register("citrine_ore",
+//            () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OVERWORLD_CITRINE_ORES.get(), 9)));
 
-    public static final RegistryObject<ConfiguredFeature<?, ?>> CITRINE_ORE = CONFIGURED_FEATURES.register("citrine_ore",
-            () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OVERWORLD_CITRINE_ORES.get(), 9)));
+    //////////////////////////////////
 
     /* public static final RegistryObject<ConfiguredFeature<?, ?>> RED_BEECH = CONFIGURED_FEATURES.register("red_beech",
             () -> new ConfiguredFeature<>(Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
@@ -45,8 +50,6 @@ public class ModConfiguredFeatures {
                             ModPlacedFeatures.RED_BEECH_CHECKED.getHolder().get()))); */
 
 
-    public static void register(IEventBus eventBus) {
-        CONFIGURED_FEATURES.register(eventBus);
-    }
+//    public static void register(IEventBus eventBus) { CONFIGURED_FEATURES.register(eventBus); }
 
 }
