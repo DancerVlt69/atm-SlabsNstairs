2640f58332545986e77fb056b2a460b6f5677b48
DancerVlt69 <dancervlt69@googlemail.com>
2023-01-21 23:32:37 +0100

    updated: ore generation | but it doesn't work right now

--- src/main/java/net/dancervlt69/slabsnstairs/init/world/features/ModConfiguredFeatures.java
+++ src/main/java/net/dancervlt69/slabsnstairs/init/world/features/ModConfiguredFeatures.java
@@ -2,57 +2,70 @@ package net.dancervlt69.slabsnstairs.init.world.features;
 
 import com.google.common.base.Suppliers;
 import net.dancervlt69.slabsnstairs.init.blocks.ModBlocks;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 import java.util.List;
 import java.util.function.Supplier;
 
 import static net.dancervlt69.slabsnstairs.SlabsNstairs.MODID;
 
-
-
 public class ModConfiguredFeatures {
 
-    public static final DeferredRegister<ConfiguredFeature<?, ?>> MOD_CONFIGURED_FEATURES =
-          DeferredRegister.create(ForgeRegistries.FEATURES.getRegistryName(), MODID);
+    public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_CITRINE_ORE_KEY = registerKey("citrine_ore");
 
-    public static final Supplier<List<OreConfiguration.TargetBlockState>> OVERWORLD_CITRINE_ORES = Suppliers.memoize(()
-            -> List.of(
-            OreConfiguration.target(new TagMatchTest(BlockTags.BASE_STONE_OVERWORLD),
-                    ModBlocks.CITRINE_ORE.get().defaultBlockState()),
-            OreConfiguration.target(new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES),
-                    ModBlocks.DEEPSLATE_CITRINE_ORE.get().defaultBlockState())));
+    // private static final ResourceKey<ConfiguredFeature<?, ?>> RED_BEECH_KEY = registerKey("red_beech");
 
-    public static final RegistryObject<ConfiguredFeature<?, ?>> CITRINE_ORE = MOD_CONFIGURED_FEATURES.register("citrine_ore",
-            () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OVERWORLD_CITRINE_ORES.get(), 9)));
+    public static final Supplier<List<OreConfiguration.TargetBlockState>> OVERWORLD_CITRINE_ORES = Suppliers.memoize(
+            () -> List.of(
+                    OreConfiguration.target(new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES),
+                            ModBlocks.CITRINE_ORE.get().defaultBlockState()),
+
+                    OreConfiguration.target(new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES),
+                            ModBlocks.DEEPSLATE_CITRINE_ORE.get().defaultBlockState())));
+
+    public static void bootstrap(BootstapContext<ConfiguredFeature<?, ?>> context) {
+        HolderGetter<PlacedFeature> placedFeatures = context.lookup(Registries.PLACED_FEATURE);
+
+        register(context, OVERWORLD_CITRINE_ORE_KEY, Feature.ORE, new OreConfiguration(OVERWORLD_CITRINE_ORES.get(),12));
+
+    }
+
+    public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
+        return ResourceKey.create(Registries.CONFIGURED_FEATURE, new ResourceLocation(MODID, name));
+    }
+
+    private static <FC extends FeatureConfiguration, F extends Feature<FC>> void register
+            (BootstapContext<ConfiguredFeature<?, ?>> context, ResourceKey<ConfiguredFeature<?, ?>> key, F feature,
+             FC configuration) { context.register(key,new ConfiguredFeature<>(feature, configuration)); }
+
+}
 
 
+//////////////////////////////////
 
-    //////////////////////////////////
+    /*
 
-    /* public static final RegistryObject<ConfiguredFeature<?, ?>> RED_BEECH = CONFIGURED_FEATURES.register("red_beech",
-            () -> new ConfiguredFeature<>(Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
-                    BlockStateProvider.simple(ModBlocks.RED_BEECH_LOG.get()),
-                    new StraightTrunkPlacer(5, 6,3),
-                    BlockStateProvider.simple(ModBlocks.RED_BEECH_LEAVES.get()),
-                    new BlobFoliagePlacer(ConstantInt.of(2),ConstantInt.of(0), 4),
-                    new TwoLayersFeatureSize(1,0,2)).build()));
+    register(context,RED_BEECH_KEY, Feature.TREE,new ConfiguredFeature<>(Feature.TREE,
+            new TreeConfiguration.TreeConfigurationBuilder(
+            BlockStateProvider.simple(ModBlocks.RED_BEECH_LOG.get()),
+            new StraightTrunkPlacer(5, 6,3),
+            BlockStateProvider.simple(ModBlocks.RED_BEECH_LEAVES.get()),
+            new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(0), 4),
+            new TwoLayersFeatureSize(1,0,2)).build()));
 
     public static final RegistryObject<ConfiguredFeature<?, ?>> RED_BEECH_SPAWN =
             CONFIGURED_FEATURES.register("red_beech_spawn", () -> new ConfiguredFeature<>(Feature.RANDOM_SELECTOR,
                     new RandomFeatureConfiguration(List.of(new WeightedPlacedFeature(
                             ModPlacedFeatures.RED_BEECH_CHECKED.getHolder().get(), 0.5f)),
                             ModPlacedFeatures.RED_BEECH_CHECKED.getHolder().get()))); */
-
-
-    public static void register(IEventBus eventBus) { MOD_CONFIGURED_FEATURES.register(eventBus); }
-
-}
--- src/main/java/net/dancervlt69/slabsnstairs/init/world/features/ModPlacedFeatures.java
+++ src/main/java/net/dancervlt69/slabsnstairs/init/world/features/ModPlacedFeatures.java
@@ -1,13 +1,15 @@
 package net.dancervlt69.slabsnstairs.init.world.features;
 
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.placement.*;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 import java.util.List;
 
@@ -15,21 +17,15 @@ import static net.dancervlt69.slabsnstairs.SlabsNstairs.MODID;
 
 public class ModPlacedFeatures {
 
-    public static final DeferredRegister<PlacedFeature> MOD_PLACED_FEATURES =
-            DeferredRegister.create(ForgeRegistries.FEATURES.getRegistryName(), MODID);
+    public static final ResourceKey<PlacedFeature> CITRINE_ORE_PLAYCED_KEY = createKey("citrine_ore");
 
-    public static final RegistryObject<PlacedFeature> CITRINE_ORE_PLACED = MOD_PLACED_FEATURES.register(
-            "citrine_ore_placed", () -> new PlacedFeature(ModConfiguredFeatures.CITRINE_ORE
-                    .getHolder().get(), commonOrePlacement(8, HeightRangePlacement
-                    .triangle(VerticalAnchor.absolute(-80), VerticalAnchor.absolute(60)))));
+    public static void bootstrap(BootstapContext<PlacedFeature> context) {
+        HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
 
-//    /* public static final RegistryObject<PlacedFeature> RED_BEECH_CHECKED = PLACED_FEATURES.register("red_beech_checked",
-//            () -> new PlacedFeature(ModConfiguredFeatures.RED_BEECH.getHolder().get(),
-//                    List.of(PlacementUtils.filteredByBlockSurvival((ModBlocks.RED_BEECH_SAPLING.get())))));
-
-//    public static final RegistryObject<PlacedFeature> RED_BEECH_PLACED = PLACED_FEATURES.register("red_beech_placed",
-//            () -> new PlacedFeature(ModConfiguredFeatures.RED_BEECH_SPAWN.getHolder().get(), VegetationPlacements.treePlacement(
-//                    PlacementUtils.countExtra(3,0.1f, 2)))); */
+        register(context, CITRINE_ORE_PLAYCED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_CITRINE_ORE_KEY),
+                commonOrePlacement(12, HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(-80),
+                        VerticalAnchor.aboveBottom(60))));
+    }
 
     public static List<PlacementModifier> orePlacement(PlacementModifier p_195347_, PlacementModifier p_195348_) {
         return List.of(p_195347_, InSquarePlacement.spread(), p_195348_, BiomeFilter.biome());
@@ -39,8 +35,28 @@ public class ModPlacedFeatures {
         return orePlacement(CountPlacement.of(p_195344_), p_195345_);
     }
 
-    public static final ResourceKey<PlacedFeature> CITRINE_ORE = PlacementUtils.createKey("citrine_ore");
+    private static ResourceKey<PlacedFeature> createKey(String name) {
+        return ResourceKey.create(Registries.PLACED_FEATURE, new ResourceLocation(MODID, name));
+    }
 
-    public static void register(IEventBus eventBus) { MOD_PLACED_FEATURES.register(eventBus); }
+    public static void register(BootstapContext<PlacedFeature> context, ResourceKey<PlacedFeature> key,
+                                Holder<ConfiguredFeature<?, ?>> configuration, List<PlacementModifier> modifiers) {
+        context.register(key, new PlacedFeature(configuration, List.copyOf(modifiers)));
+    }
+
+    public static void register(BootstapContext<PlacedFeature> context, ResourceKey<PlacedFeature> key,
+                                Holder<ConfiguredFeature<?, ? >> configuration, PlacementModifier... modifiers) {
+        register(context, key, configuration, List.of(modifiers));
+    }
 
 }
+
+
+
+//    /* public static final RegistryObject<PlacedFeature> RED_BEECH_CHECKED = PLACED_FEATURES.register("red_beech_checked",
+//            () -> new PlacedFeature(ModConfiguredFeatures.RED_BEECH.getHolder().get(),
+//                    List.of(PlacementUtils.filteredByBlockSurvival((ModBlocks.RED_BEECH_SAPLING.get())))));
+
+//    public static final RegistryObject<PlacedFeature> RED_BEECH_PLACED = PLACED_FEATURES.register("red_beech_placed",
+//            () -> new PlacedFeature(ModConfiguredFeatures.RED_BEECH_SPAWN.getHolder().get(), VegetationPlacements.treePlacement(
+//                    PlacementUtils.countExtra(3,0.1f, 2)))); */
